39ee0f1a1720788f6e864dc98dea32bd9af5c7c0
Divansh Singh <divanshsinghsaini123@gmail.com>
2026-04-19 03:14:00 -0800

    added more things in schema

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -377,8 +377,10 @@ export interface SharedStore extends Struct.ComponentSchema {
   };
   attributes: {
     address: Schema.Attribute.String & Schema.Attribute.Required;
+    email: Schema.Attribute.Email & Schema.Attribute.Required;
     latitude: Schema.Attribute.String & Schema.Attribute.Required;
     longitude: Schema.Attribute.String & Schema.Attribute.Required;
+    mobileNumber: Schema.Attribute.String & Schema.Attribute.Required;
     name: Schema.Attribute.String & Schema.Attribute.Required;
     pincode: Schema.Attribute.String & Schema.Attribute.Required;
     state: Schema.Attribute.String & Schema.Attribute.Required;
