927cdf8127bf81c56dadc26e2593113b2aceac92
Divansh Singh <divanshsinghsaini123@gmail.com>
2026-05-15 23:35:51 -0800

    added gcore for media files

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,35 @@
-export default () => ({});
+module.exports = ({ env }) => ({
+  upload: {
+    config: {
+      provider: 'aws-s3',
+
+      providerOptions: {
+        // CDN URL
+        baseUrl: env('GCORE_CDN_URL'),
+
+        s3Options: {
+          credentials: {
+            accessKeyId: env('GCORE_ACCESS_KEY_ID'),
+            secretAccessKey: env('GCORE_SECRET_ACCESS_KEY'),
+          },
+
+          endpoint: env('GCORE_ENDPOINT'),
+          region: env('GCORE_REGION'),
+
+          params: {
+            Bucket: env('GCORE_BUCKET_NAME'),
+
+            // Folder inside bucket
+            Prefix: 'strapi/uploads/',
+          },
+        },
+      },
+
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+    },
+  },
+});
