6f0c79333bb6144c776248fec26848602f52579a
Divansh Singh <divanshsinghsaini123@gmail.com>
2026-06-18 05:46:03 -0400

    increased the session time

--- config/admin.ts
+++ config/admin.ts
@@ -1,6 +1,14 @@
 export default ({ env }) => ({
   auth: {
     secret: env('ADMIN_JWT_SECRET'),
+    sessions: {
+      // Keep the admin token alive long enough for slow Content-Type Builder work.
+      accessTokenLifespan: env.int('ADMIN_ACCESS_TOKEN_LIFESPAN', 8 * 60 * 60),
+      idleSessionLifespan: env.int('ADMIN_IDLE_SESSION_LIFESPAN', 8 * 60 * 60),
+      maxSessionLifespan: env.int('ADMIN_MAX_SESSION_LIFESPAN', 24 * 60 * 60),
+      idleRefreshTokenLifespan: env.int('ADMIN_IDLE_REFRESH_TOKEN_LIFESPAN', 14 * 24 * 60 * 60),
+      maxRefreshTokenLifespan: env.int('ADMIN_MAX_REFRESH_TOKEN_LIFESPAN', 30 * 24 * 60 * 60),
+    },
   },
   apiToken: {
     salt: env('API_TOKEN_SALT'),
