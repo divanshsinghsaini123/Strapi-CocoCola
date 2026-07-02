53a0baf3da92a09504f3b8b068f22497cc1d7bbe
Divansh Singh <divanshsinghsaini123@gmail.com>
2026-07-02 10:23:37 -0400

    fixed thumbnet issue

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,7 +1,21 @@
+const gcoreCdnUrl = process.env.GCORE_CDN_URL || '';
+
 export default [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'img-src': gcoreCdnUrl ? ["'self'", 'data:', gcoreCdnUrl] : ["'self'", 'data:'],
+          'media-src': gcoreCdnUrl ? ["'self'", 'data:', gcoreCdnUrl] : ["'self'", 'data:'],
+          'connect-src': gcoreCdnUrl ? ["'self'", gcoreCdnUrl] : ["'self'"],
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
